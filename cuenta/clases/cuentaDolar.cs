5fa87a2132c8062b9a00b2ebb346ccb7f4e219f0
Gonzalo <querer44@gmail.com>
2022-09-23 17:20:35 -0300

    primer solucion

---
+++ cuenta/clases/cuentaDolar.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace cuenta
+{
+    public class CuentaDolar
+    {
+        private int capital;
+           
+            
+        public cliente(int capital){
+            this.capital = capita;
+        }
+
+        public string extraccion(int monto, int tipoOperacion){
+            
+            if (operacion == 1 && monto <= 200)
+                return descontar(monto);
+            else
+                return descontar(monto);
+        }
+
+        private string descontarCajero(int monto){
+                if(monto < capital){
+                    capita -= monto;
+                    return "monto restante " + capital;
+                }     
+                else
+                    return "el monto solisitado es mayor al que se disponbe actualmente";
+        }
+
+        public string insercion(int monto){
+            this.capital += monto;
+            return "ingresado " + monto + "cuenta actual" capita;
+
+        }
+}
