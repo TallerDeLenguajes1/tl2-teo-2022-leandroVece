f10f015c8d1840e47ccaed89633a4c14e8dbe9cc
Gonzalo <querer44@gmail.com>
2022-09-23 19:25:22 -0300

    error de herencia resuelto

--- cuenta/Program.cs
+++ cuenta/Program.cs
@@ -6,9 +6,33 @@ namespace cuenta
     {
         static void Main(string[] args)
         {
-            CuentaAhorro newCuenta = new CuentaAhorro(800);
+
+            int option;
+            cuentaBase newCuenta = null;
+            
+            Console.WriteLine("Tipo de cuenta que quiere crear 1) ahorro 2) Pesos 3) dolares");
+            
+            option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    newCuenta = new CuentaAhorro(500);
+                    break;
+                case 2:
+                    newCuenta = new CuentaPeso(500);
+                    break;
+                case 3:
+                    newCuenta = new CuentaDolar(500);
+                    break;
+                default:
+                    break;
+                     Console.WriteLine("opcion incorrecta");
+
+            }
 
             Console.WriteLine(newCuenta.ingresar(200));
+            
         }
     }
 }
